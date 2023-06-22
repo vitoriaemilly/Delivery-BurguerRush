eaa914e9d5e0a10fe0bdb3f238aff0cf1edf8834
Pedro Henrique <121135540+PedroCod3r@users.noreply.github.com>
2023-06-22 00:39:53 -0300

    upadate
    update arquivo receber dados e adição de mascaras

--- receberDados.php
+++ receberDados.php
@@ -14,17 +14,25 @@
      $telefone = $_POST['telefone'];
      $senha = $_POST['senha'];  
          
- 
-     $sql = "INSERT INTO usuario (nome, cpf, email, telefone, senha, estado, cidade, bairro, numero, complemento, cep)  VALUES ('{$nomeUsuario}', '{$cpf}', '{$email}', '{$telefone}', '{$senha}', '{$estado}', '{$cidade}', '{$bairro}', '{$numero}', '{$complemento}', '{$cep}')";
+
+    $sql_validacao = "SELECT * FROM usuario WHERE cpf = '$cpf'";
+    $result_validacao = $conn->query($sql_validacao);
+
+    if ($result_validacao->num_rows > 0) {
+        // Já existe um usuário com o mesmo número de CPF
+        echo "<script>alert('Já existe um usuário com o CPF informado.');
+        window.location='login.php';</script>";
+    } else {
+        $sql = "INSERT INTO usuario (nome, cpf, email, telefone, senha, estado, cidade, bairro, numero, complemento, cep)  VALUES ('{$nomeUsuario}', '{$cpf}', '{$email}', '{$telefone}', '{$senha}', '{$estado}', '{$cidade}', '{$bairro}', '{$numero}', '{$complemento}', '{$cep}')";
          
          
-     if(mysqli_query($conn,$sql)){
-         echo "<script>alert('Usuário cadastrado!');
-         window.location='index.php';</script>"; // Cadastro realizado com sucesso
-         }else{
-             echo"<script> alert('Erro ao cadastrar'); </script>"; // Erro ao cadastrar
-         }   
-               
+            if(mysqli_query($conn,$sql)){
+                echo "<script>alert('Usuário cadastrado!');
+                window.location='index.php';</script>"; // Cadastro realizado com sucesso
+                }else{
+                    echo"<script> alert('Erro ao cadastrar'); </script>"; // Erro ao cadastrar
+            }   
+    }          
      mysqli_close($conn);
  
  
