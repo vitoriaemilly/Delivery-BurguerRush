17ed7350220c031f377a82a7625368c081a45929
Pedro Henrique <121135540+PedroCod3r@users.noreply.github.com>
2023-06-22 00:51:12 -0300

    update errado

--- fazerLogin.php
+++ fazerLogin.php
@@ -4,16 +4,12 @@
     $email = $_POST['email'];
     $senha = $_POST['password'];
 
+    // $sql_nome = "SELECT nome FROM usuarios WHERE email = '$email'";
     $sql = "SELECT * FROM usuario WHERE email = '$email' AND senha = '$senha'";
     $result = $conn->query($sql);
-    
-    // $sql_nome = "SELECT nome FROM usuario WHERE email = '$email'";
-    // $result2 = $conn->query($sql_nome);
-    
-    if ($result->num_rows > 0) {
-        // if ($result2->num_rows > 0) {
+
+        if ($result->num_rows > 0) {
             echo "<script>alert('Login realizado com Sucesso, você foi logado como {$email}');window.location='index.php';</script>"; // Login bem-sucedido
-        // }
     } else {
             echo "<script>alert('Desculpe, credenciais invalidas');window.location='login.php'</script>";// Dados de login inválidos
     }
